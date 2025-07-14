398e332d2808387ec0f60c16b0cb9fcc3b5ac558
Serge Frederick Possono Moumbana <possonom@users.noreply.github.com>
2025-07-14 23:30:53 +0200

    Initial commit

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,4 +1,4 @@
-  import * as React from 'react';
+import * as React from 'react';
   import { useState } from 'react';
   import { IHelloWorldProps } from './IHelloWorldProps';
   import { CommandBar, ICommandBarItemProps, TextField, ThemeProvider, } from '@fluentui/react';
